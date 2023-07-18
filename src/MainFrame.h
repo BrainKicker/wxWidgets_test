6b4bec7b0ab43fc7584bd8b35efd12372588deed
BrainKicker <peter.sabanov@gmail.com>
2023-07-18 20:36:48 +0300

    Initial commit

---
+++ src/MainFrame.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <wx/wx.h>
+
+
+class MainFrame : public wxFrame {
+
+public:
+    explicit MainFrame(const wxString & title);
+
+private:
+    void OnHello(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+};
